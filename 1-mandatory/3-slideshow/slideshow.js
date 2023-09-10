3e56141d070e30bd35925c8381fe068a7d052430
oleks211 <aleksandr.savelyev@gmail.com>
2023-09-10 19:39:40 +0300

    complete extra

--- 1-mandatory/3-slideshow/slideshow.js
+++ 1-mandatory/3-slideshow/slideshow.js
@@ -1,50 +1,78 @@
-const images = ["assets/cute-cat-a.png", "assets/cute-cat-b.jpg", "assets/cute-cat-c.jpg", "assets/cute-cat-d.png"];
+const images = [
+  "./assets/cute-cat-a.png",
+  "./assets/cute-cat-b.jpg",
+  "./assets/cute-cat-c.jpg",
+];
 let currentIndex = 0;
 let autoPlayInterval;
+let intervalSeconds = 2;
 
-const imageElement = document.getElementById('image');
-const prevButton = document.getElementById('prev-button');
-const nextButton = document.getElementById('next-button');
-const autoPrevButton = document.getElementById('auto-prev-button');
-const autoNextButton = document.getElementById('auto-next-button');
+const imageElement = document.getElementById("carousel-img");
+const prevButton = document.getElementById("backward-btn");
+const nextButton = document.getElementById("forward-btn");
+const autoPrevButton = document.getElementById("auto-backward");
+const autoNextButton = document.getElementById("auto-forward");
+const intervalInput = document.getElementById("interval-input");
+const setIntervalButton = document.getElementById("set-interval-button");
+const stopButton = document.getElementById("stop");
 
 function showImage(index) {
-    if (index >= 0 && index < images.length) {
-        imageElement.src = images[index];
-        currentIndex = index;
-    }
+  if (index >= 0 && index < images.length) {
+    imageElement.src = images[index];
+    currentIndex = index;
+  }
 }
 
-prevButton.addEventListener('click', () => {
-    showImage(currentIndex - 1);
+prevButton.addEventListener("click", () => {
+  var imageNumber = currentIndex === 0 ? images.length - 1 : currentIndex - 1;
+  showImage(imageNumber);
 });
 
-nextButton.addEventListener('click', () => {
-    showImage(currentIndex + 1);
+nextButton.addEventListener("click", () => {
+  var imageNumber = currentIndex + 1 === images.length ? 0 : currentIndex + 1;
+  showImage(imageNumber);
 });
 
-autoPrevButton.addEventListener('click', () => {
-    autoPlay(false);
+autoPrevButton.addEventListener("click", () => {
+  autoPrevButton.disabled = true;
+  autoNextButton.disabled = true;
+  autoPlay(false);
 });
 
-autoNextButton.addEventListener('click', () => {
-    autoPlay(true);
+autoNextButton.addEventListener("click", () => {
+  autoPrevButton.disabled = true;
+  autoNextButton.disabled = true;
+  autoPlay(true);
+});
+
+stopButton.addEventListener("click", () => {
+  clearInterval(autoPlayInterval);
+  autoPrevButton.disabled = false;
+  autoNextButton.disabled = false;
+});
+
+setIntervalButton.addEventListener("click", () => {
+  intervalSeconds = parseFloat(intervalInput.value);
+  clearInterval(autoPlayInterval);
+  autoPlay(true);
 });
 
 function autoPlay(next) {
-    clearInterval(autoPlayInterval);
+  clearInterval(autoPlayInterval);
+  autoPlayInterval = setInterval(() => {
     if (next) {
-        showImage(currentIndex + 1);
+      currentIndex = currentIndex + 1 === images.length ? 0 : currentIndex + 1;
     } else {
-        showImage(currentIndex - 1);
+      currentIndex = currentIndex === 0 ? images.length - 1 : currentIndex - 1;
     }
-    autoPlayInterval = setInterval(() => {
-        showImage(next ? currentIndex + 1 : currentIndex - 1);
-    }, 3000);
+    showImage(currentIndex);
+  }, intervalSeconds * 1000);
 }
 
 showImage(currentIndex);
 
 autoPlayInterval = setInterval(() => {
-    showImage(currentIndex + 1);
-}, 3000);
+  currentIndex = images.length === currentIndex + 1 ? 0 : currentIndex + 1;
+  showImage(currentIndex);
+}, intervalSeconds * 1000);
+// npm test run 1-mandatory/3-slideshow/slideshow.test.js
